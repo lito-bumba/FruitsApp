5d8b547b8385d08fb6048e6a1628cb13790b48ac
“Lito <litocdbumba@gmail.com>
2024-03-06 12:05:29 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Cazombo Bumba on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
